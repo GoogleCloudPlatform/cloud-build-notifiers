72fab6d1db75b8171cd3cef80cd261552e41568c
Aric1088 <ariczhuang@gmail.com>
2020-06-30 12:48:18 -0400

    added ensurefunction tests

--- bigquery/go.mod
+++ bigquery/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.4.0
 	github.com/google/go-cmp v0.4.0
+	google.golang.org/api v0.15.0
 	google.golang.org/genproto v0.0.0-20200413115906-b5235f65be36
 	gopkg.in/yaml.v2 v2.2.8
 )
--- bigquery/main_test.go
+++ bigquery/main_test.go
@@ -16,9 +16,14 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"testing"
 
+	"cloud.google.com/go/bigquery"
 	"github.com/GoogleCloudPlatform/cloud-build-notifiers/lib/notifiers"
+	log "github.com/golang/glog"
+	"google.golang.org/api/googleapi"
 )
 
 type mockBQ struct {
@@ -31,11 +36,65 @@ func (bqf *mockBQFactory) Make(ctx context.Context) (bq, error) {
 	return &mockBQ{}, nil
 }
 
+var fakeBQServerDS = map[string]fakeDMResponse{
+	"dne":     {&bigquery.DatasetMetadata{}, &googleapi.Error{Code: 404, Message: "not found"}},
+	"noauth":  {&bigquery.DatasetMetadata{}, &googleapi.Error{Code: 403, Message: "no authorization"}},
+	"broke":   {&bigquery.DatasetMetadata{}, &googleapi.Error{Code: 500, Message: "bq server error"}},
+	"strange": {&bigquery.DatasetMetadata{}, &googleapi.Error{Code: 305, Message: "use proxy"}},
+}
+
+var fakeBQServerTable = map[string]fakeTMResponse{
+	"dne":     {&bigquery.TableMetadata{}, &googleapi.Error{Code: 404, Message: "not found"}},
+	"noauth":  {&bigquery.TableMetadata{}, &googleapi.Error{Code: 403, Message: "no authorization"}},
+	"broke":   {&bigquery.TableMetadata{}, &googleapi.Error{Code: 500, Message: "bq server error"}},
+	"strange": {&bigquery.TableMetadata{}, &googleapi.Error{Code: 305, Message: "use proxy"}},
+}
+
+type fakeDMResponse struct {
+	dataset   *bigquery.DatasetMetadata
+	fakeError error
+}
+
+type fakeTMResponse struct {
+	table     *bigquery.TableMetadata
+	fakeError error
+}
+
 func (bq *mockBQ) EnsureDataset(ctx context.Context, datasetName string) error {
+	fakeResponse, _ := fakeBQServerDS[datasetName]
+	err := fakeResponse.fakeError
+	if err != nil {
+		log.Warningf("Error obtaining dataset metadata: %v", err)
+		if strings.Contains(err.Error(), "404") {
+			return nil
+		}
+		if strings.Contains(err.Error(), "403") {
+			return fmt.Errorf("Error creating dataset: %v", err.Error())
+		}
+		if strings.Contains(err.Error(), "500") {
+			return fmt.Errorf("Error creating dataset: %v", err.Error())
+		}
+		return fmt.Errorf("Encountered error %v: ", err.Error())
+	}
 	return nil
 }
 
 func (bq *mockBQ) EnsureTable(ctx context.Context, tableName string) error {
+	fakeResponse, _ := fakeBQServerTable[tableName]
+	err := fakeResponse.fakeError
+	if err != nil {
+		log.Warningf("Error obtaining table metadata: %v", err)
+		if strings.Contains(err.Error(), "404") {
+			return nil
+		}
+		if strings.Contains(err.Error(), "403") {
+			return fmt.Errorf("Failed to initialize table %v: ", err.Error())
+		}
+		if strings.Contains(err.Error(), "500") {
+			return fmt.Errorf("Failed to initialize table %v: ", err.Error())
+		}
+		return fmt.Errorf("Encountered error %v: ", err.Error())
+	}
 	return nil
 }
 
@@ -127,6 +186,134 @@ func TestSetUp(t *testing.T) {
 		},
 		wantErr: true,
 	}} {
+		t.Run(tc.name, func(t *testing.T) {
+			n := &bqNotifier{bqf: &mockBQFactory{}}
+			err := n.SetUp(context.Background(), tc.cfg, nil)
+			if err != nil {
+				if tc.wantErr {
+					t.Logf("got expected error: %v", err)
+					return
+				}
+				t.Fatalf("SetUp(%v) got unexpected error: %v", tc.cfg, err)
+			}
+
+			if tc.wantErr {
+				t.Error("unexpected success")
+			}
+
+		})
+	}
+}
+
+func TestEnsureFunctions(t *testing.T) {
+
+	for _, tc := range []struct {
+		name    string
+		cfg     *notifiers.Config
+		wantErr bool
+	}{{
+		name: "valid dataset and table, functional BQ",
+		cfg: &notifiers.Config{
+			Spec: &notifiers.Spec{
+				Notification: &notifiers.Notification{
+					Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+					Delivery: map[string]interface{}{
+						"table": "projects/project_name/datasets/dataset_name/tables/table_name",
+					},
+				},
+			},
+		},
+	}, {
+		name: "create nonexistent dataset",
+		cfg: &notifiers.Config{
+			Spec: &notifiers.Spec{
+				Notification: &notifiers.Notification{
+					Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+					Delivery: map[string]interface{}{
+						"table": "projects/project_name/datasets/dne/tables/table_name",
+					},
+				},
+			},
+		},
+	}, {
+		name: "create nonexistent table",
+		cfg: &notifiers.Config{
+			Spec: &notifiers.Spec{
+				Notification: &notifiers.Notification{
+					Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+					Delivery: map[string]interface{}{
+						"table": "projects/project_name/datasets/dataset_name/tables/dne",
+					},
+				},
+			},
+		},
+	}, {
+		name: "no perm dataset",
+		cfg: &notifiers.Config{
+			Spec: &notifiers.Spec{
+				Notification: &notifiers.Notification{
+					Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+					Delivery: map[string]interface{}{
+						"table": "projects/project_name/datasets/noauth/tables/table_name",
+					},
+				},
+			},
+		},
+		wantErr: true,
+	}, {
+		name: "no perm table",
+		cfg: &notifiers.Config{
+			Spec: &notifiers.Spec{
+				Notification: &notifiers.Notification{
+					Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+					Delivery: map[string]interface{}{
+						"table": "projects/project_name/datasets/dataset_name/tables/noauth",
+					},
+				},
+			},
+		},
+		wantErr: true,
+	}, {
+		name: "server error",
+		cfg: &notifiers.Config{
+			Spec: &notifiers.Spec{
+				Notification: &notifiers.Notification{
+					Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+					Delivery: map[string]interface{}{
+						"table": "projects/project_name/datasets/dataset_name/tables/broke",
+					},
+				},
+			},
+		},
+		wantErr: true,
+	}, {
+		name: "some non 200 error",
+		cfg: &notifiers.Config{
+			Spec: &notifiers.Spec{
+				Notification: &notifiers.Notification{
+					Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+					Delivery: map[string]interface{}{
+						"table": "projects/project_name/datasets/dataset_name/tables/strange",
+					},
+				},
+			},
+		},
+		wantErr: true,
+	},
+		{
+			name: "more server error",
+			cfg: &notifiers.Config{
+				Spec: &notifiers.Spec{
+					Notification: &notifiers.Notification{
+						Filter: `build.build_trigger_id == "123e4567-e89b-12d3-a456-426614174000" `,
+						Delivery: map[string]interface{}{
+							"table": "projects/project_name/datasets/broke/tables/table_name",
+						},
+					},
+				},
+			},
+			wantErr: true,
+		}} {
 		t.Run(tc.name, func(t *testing.T) {
 			n := &bqNotifier{bqf: &mockBQFactory{}}
 			err := n.SetUp(context.Background(), tc.cfg, nil)
