2b9e073d717cb5127965a6cfbaa7a897012207d9
Brett Nekolny <brett.nekolny@brandfolder.com>
2021-09-15 12:36:42 -0600

    Early implementation toward updating messages. (#6)
    * Early implementation toward updating messages.
    
    * closer to functional.
    
    * Update comment.
    
    * grab api key from config.
    
    * fix a print statement.
    
    * add logging.
    
    * Infof
    
    * messed up conditional, ==
    
    * add some logging.
    
    * Format error as string.
    
    * one more log line.
    
    * another round of prints.
    
    * Remove quoting from object.
    
    * plain string
    
    * Update some things, add some tests.
    
    * gofmt.
    
    * Try a different write method.
    
    * fprintf -> fprint
    
    * more prints.
    
    * print bytes.
    
    * clean up, it works :shrug:
    
    * Delete the file after we're done

--- slack/main.go
+++ slack/main.go
@@ -18,6 +18,11 @@ import (
 	"context"
 	"fmt"
 
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/cloud-build-notifiers/lib/notifiers"
 	log "github.com/golang/glog"
 	"github.com/slack-go/slack"
@@ -27,6 +32,8 @@ import (
 const (
 	webhookURLSecretName          = "webhookUrl"
 	notificationChannelConfigName = "notificationChannel"
+	apiTokenSecretName            = "apiToken"
+	storagePathPrefixf            = "messages/%s"
 )
 
 func main() {
@@ -40,6 +47,9 @@ type slackNotifier struct {
 
 	webhookURL          string
 	notificationChannel string
+	apiToken            string
+
+	storageBucket string
 }
 
 func (s *slackNotifier) SetUp(ctx context.Context, cfg *notifiers.Config, sg notifiers.SecretGetter, _ notifiers.BindingResolver) error {
@@ -68,24 +78,114 @@ func (s *slackNotifier) SetUp(ctx context.Context, cfg *notifiers.Config, sg not
 		s.notificationChannel, _ = channelRef.(string)
 	}
 
+	apiRef, err := notifiers.GetSecretRef(cfg.Spec.Notification.Delivery, apiTokenSecretName)
+	if err != nil {
+		return fmt.Errorf("failed to get Secret ref from delivery config (%v) field %q: %w", cfg.Spec.Notification.Delivery, apiTokenSecretName, err)
+	}
+	apiResource, err := notifiers.FindSecretResourceName(cfg.Spec.Secrets, apiRef)
+	if err != nil {
+		return fmt.Errorf("failed to find Secret for ref %q: %w", apiRef, err)
+	}
+	apiToken, err := sg.GetSecret(ctx, apiResource)
+	if err != nil {
+		return fmt.Errorf("failed to get api secret: %w", err)
+	}
+	s.apiToken = apiToken
+
+	cfgPath := os.Getenv("CONFIG_PATH")
+	if trm := strings.TrimPrefix(cfgPath, "gs://"); trm != cfgPath {
+		cfgPath = trm
+		split := strings.SplitN(cfgPath, "/", 2)
+		s.storageBucket = split[0]
+	}
+
 	return nil
 }
 
-func (s *slackNotifier) SendNotification(ctx context.Context, build *cbpb.Build) error {
+func (s *slackNotifier) SendNotification(ctx context.Context, build *cbpb.Build) (err error) {
 	if !s.filter.Apply(ctx, build) {
 		return nil
 	}
 
-	log.Infof("sending Slack webhook for Build %q (status: %q)", build.Id, build.Status)
-	msg, err := s.writeMessage(build)
-	if err != nil {
-		return fmt.Errorf("failed to write Slack message: %w", err)
-	}
+	slackClient := slack.New(s.apiToken)
 
-	return slack.PostWebhook(s.webhookURL, msg)
+	log.Infof("sending Slack webhook for Build %q (status: %q)", build.Id, build.Status)
+	attachmentMsgOpt := s.buildAttachmentMessageOption(build)
+	timestamp := s.getTimestamp(build.Id)
+	if timestamp == "" {
+		_, timestamp, err = slackClient.PostMessage(s.notificationChannel, *attachmentMsgOpt)
+		s.setTimestamp(build.Id, timestamp)
+	} else {
+		_, _, _, err = slackClient.UpdateMessage(s.notificationChannel, timestamp, *attachmentMsgOpt)
+		s.deleteTimestamp(build.Id)
+	}
+	return
 }
 
-func (s *slackNotifier) writeMessage(build *cbpb.Build) (*slack.WebhookMessage, error) {
+func (s *slackNotifier) getStoragePath(buildId string) string {
+	return fmt.Sprintf(storagePathPrefixf, buildId)
+}
+
+func (s *slackNotifier) deleteTimestamp(buildId string) {
+	sc, err := storage.NewClient(context.Background())
+	if err != nil {
+		return
+	}
+	defer sc.Close()
+
+	if err := sc.Bucket(s.storageBucket).Object(s.getStoragePath(buildId)).Delete(context.Background()); err != nil {
+		log.Infof("Error deleting the object: %q", err.Error())
+		return
+	}
+	return
+}
+
+func (s *slackNotifier) setTimestamp(buildId string, timestamp string) {
+	sc, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Infof("Unable to create storage client: %q", err.Error())
+		return
+	}
+	defer sc.Close()
+
+	writer := sc.Bucket(s.storageBucket).Object(s.getStoragePath(buildId)).NewWriter(context.Background())
+	defer func() {
+		err := writer.Close()
+		if err != nil {
+			log.Infof("Error closing the writer: %q", err.Error())
+		}
+	}()
+	if _, err := fmt.Fprint(writer, timestamp); err != nil {
+		return
+	}
+}
+
+func (s *slackNotifier) getTimestamp(buildId string) (timestamp string) {
+	sc, err := storage.NewClient(context.Background())
+	if err != nil {
+		return
+	}
+	defer sc.Close()
+
+	path := s.getStoragePath(buildId)
+	//r, err := sc.NewReader(ctx, bucket, path)
+	reader, err := sc.Bucket(s.storageBucket).Object(path).NewReader(context.Background())
+	if err != nil {
+		return
+	}
+	defer reader.Close()
+
+	var b []byte
+	// will move to "io" in golang 1.16
+	if b, err = ioutil.ReadAll(reader); err != nil {
+		return
+	}
+	timestamp = string(b)
+
+	return
+}
+
+func (s *slackNotifier) buildAttachmentMessageOption(build *cbpb.Build) *slack.MsgOption {
 	repoName, ok := build.Substitutions["REPO_NAME"]
 	if !ok {
 		repoName = "UNKNOWN_REPO"
@@ -113,7 +213,7 @@ func (s *slackNotifier) writeMessage(build *cbpb.Build) (*slack.WebhookMessage, 
 
 	logURL, err := notifiers.AddUTMParams(build.LogUrl, notifiers.ChatMedium)
 	if err != nil {
-		return nil, fmt.Errorf("failed to add UTM params: %w", err)
+		logURL = build.LogUrl
 	}
 
 	txt := fmt.Sprintf(
@@ -140,13 +240,11 @@ func (s *slackNotifier) writeMessage(build *cbpb.Build) (*slack.WebhookMessage, 
 		clr = "warning"
 	}
 
-	atch := slack.Attachment{
+	attachment := slack.Attachment{
 		Text:  txt,
 		Color: clr,
 	}
 
-	return &slack.WebhookMessage{
-		Attachments: []slack.Attachment{atch},
-		Channel:     s.notificationChannel,
-	}, nil
+	attachmentMsgOption := slack.MsgOptionAttachments(attachment)
+	return &attachmentMsgOption
 }
--- slack/main_test.go
+++ slack/main_test.go
@@ -2,45 +2,56 @@ package main
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/slack-go/slack"
+	cbpb "google.golang.org/genproto/googleapis/devtools/cloudbuild/v1"
 )
 
-func TestWriteMessage(t *testing.T) {
-	//n := new(slackNotifier)
-	//b := &cbpb.Build{
-	//	ProjectId: "my-project-id",
-	//	Id:        "some-build-id",
-	//	Status:    cbpb.Build_SUCCESS,
-	//	LogUrl:    "https://some.example.com/log/url?foo=bar",
-	//	SourceProvenance: &cbpb.SourceProvenance{
-	//		ResolvedRepoSource: &cbpb.RepoSource{
-	//			RepoName: "test-repo",
-	//			Revision: &cbpb.RepoSource_BranchName{
-	//				BranchName: "test-branch",
-	//			},
-	//		},
-	//	},
-	//}
-	//
-	//got, err := n.writeMessage(b)
-	//if err != nil {
-	//	t.Fatalf("writeMessage failed: %v", err)
-	//}
-	//
-	//want := &slack.WebhookMessage{
-	//	Attachments: []slack.Attachment{{
-	//		//Text:  "Cloud Build (my-project-id, some-build-id): SUCCESS",
-	//		Text:  ":test-repo: test-repo SUCCESS (my-project-id) \n test-branch",
-	//		Color: "good",
-	//		Actions: []slack.AttachmentAction{{
-	//			Text: "View Logs",
-	//			Type: "button",
-	//			URL:  "https://some.example.com/log/url?foo=bar&utm_campaign=google-cloud-build-notifiers&utm_medium=chat&utm_source=google-cloud-build",
-	//		}},
-	//	}},
-	//}
-	//
-	//if diff := cmp.Diff(got, want); diff != "" {
-	//	t.Errorf("writeMessage got unexpected diff: %s", diff)
-	//}
+func TestGetStoragePath(t *testing.T) {
+	n := new(slackNotifier)
+	result := n.getStoragePath("build-id")
+	expected := "messages/build-id"
+	if result != expected {
+		t.Errorf("Unexpected storage path: %q, expected: %q", result, expected)
+	}
+	return
+}
+
+func TestBuildAttachmentMessageOption(t *testing.T) {
+	n := new(slackNotifier)
+	b := &cbpb.Build{
+		ProjectId: "my-project-id",
+		Id:        "some-build-id",
+		Status:    cbpb.Build_SUCCESS,
+		LogUrl:    "https://some.example.com/log/url?foo=bar",
+		SourceProvenance: &cbpb.SourceProvenance{
+			ResolvedRepoSource: &cbpb.RepoSource{
+				RepoName: "test-repo",
+				Revision: &cbpb.RepoSource_BranchName{
+					BranchName: "test-branch",
+				},
+			},
+		},
+	}
+
+	got := n.buildAttachmentMessageOption(b)
+
+	want := slack.MsgOptionAttachments(
+		slack.Attachment{
+			Text:  "SUCCESS: :UNKNOWN_REPO: UNKNOWN_REPO (my-project-id) \u003chttps://some.example.com/log/url?foo=bar\u0026utm_campaign=google-cloud-build-notifiers\u0026utm_medium=chat\u0026utm_source=google-cloud-build|View Build\u003e\n*Branch*: UNKNOWN_BRANCH *Author*: UNKNOWN_COMMIT_AUTHOR \n\u003cUNKNOWN_COMMIT_URL|Commit\u003e *UNKNOWN_COMMIT_SHA*: UNKNOWN_COMMIT_MESSAGE",
+			Color: "good",
+		},
+	)
+
+	_, gotValues, err := slack.UnsafeApplyMsgOptions("fake-token", "fake-channel", "https://fake.com/", *got)
+	if err != nil {
+		t.Errorf("Unable to build message: %w", err)
+	}
+	_, wantValues, err := slack.UnsafeApplyMsgOptions("fake-token", "fake-channel", "https://fake.com/", want)
+	if diff := cmp.Diff(gotValues, wantValues); diff != "" {
+		t.Logf("full message: %v", gotValues)
+		t.Errorf("writeMessage got unexpected diff: %s", diff)
+	}
 	return
 }
