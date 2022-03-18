5871851fc9d6db0ec48fa376ef259784c6c64ee9
Katie McLaughlin <katie@glasnt.com>
2022-03-18 13:54:12 +1100

    Add unit tests

--- googlechat/main_test.go
+++ googlechat/main_test.go
@@ -2,38 +2,69 @@ package main
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	chat "google.golang.org/api/chat/v1"
+	cbpb "google.golang.org/genproto/googleapis/devtools/cloudbuild/v1"
 )
 
 func TestWriteMessage(t *testing.T) {
-	/*
-		TODO: write tests.
-		n := new(slackNotifier)
-		b := &cbpb.Build{
-			ProjectId: "my-project-id",
-			Id:        "some-build-id",
-			Status:    cbpb.Build_SUCCESS,
-			LogUrl:    "https://some.example.com/log/url?foo=bar",
-		}
 
-		got, err := n.writeMessage(b)
-		if err != nil {
-			t.Fatalf("writeMessage failed: %v", err)
-		}
+	n := new(googlechatNotifier)
+	b := &cbpb.Build{
+		ProjectId: "my-project-id",
+		Id:        "some-build-id",
+		Status:    cbpb.Build_SUCCESS,
+		LogUrl:    "https://some.example.com/log/url?foo=bar",
+	}
 
-		want := &slack.WebhookMessage{
-			Attachments: []slack.Attachment{{
-				Text:  "Cloud Build (my-project-id, some-build-id): SUCCESS",
-				Color: "good",
-				Actions: []slack.AttachmentAction{{
-					Text: "View Logs",
-					Type: "button",
-					URL:  "https://some.example.com/log/url?foo=bar&utm_campaign=google-cloud-build-notifiers&utm_medium=chat&utm_source=google-cloud-build",
-				}},
-			}},
-		}
+	got, err := n.writeMessage(b)
+	if err != nil {
+		t.Fatalf("writeMessage failed: %v", err)
+	}
 
-		if diff := cmp.Diff(got, want); diff != "" {
-			t.Errorf("writeMessage got unexpected diff: %s", diff)
-		}
-	*/
+	want := &chat.Message{
+		Cards: []*chat.Card{{
+			Header: &chat.CardHeader{
+				ImageUrl: "https://www.gstatic.com/images/icons/material/system/2x/check_circle_googgreen_48dp.png",
+				Subtitle: "my-project-id",
+				Title:    "Build some-bui Status: SUCCESS",
+			},
+			Sections: []*chat.Section{
+				{
+					Widgets: []*chat.WidgetMarkup{
+						{
+							KeyValue: &chat.KeyValue{
+								TopLabel: "Duration",
+								Content:  "0 min 0 sec",
+							},
+						},
+					},
+				},
+				{
+					Widgets: []*chat.WidgetMarkup{
+						{
+							Buttons: []*chat.Button{
+								{
+									TextButton: &chat.TextButton{
+										Text: "open logs",
+										OnClick: &chat.OnClick{
+											OpenLink: &chat.OpenLink{
+												Url: "https://some.example.com/log/url?foo=bar&utm_campaign=google-cloud-build-notifiers&utm_medium=chat&utm_source=google-cloud-build",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		}}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("writeMessage got unexpected diff: %s", diff)
+	}
+
 }
