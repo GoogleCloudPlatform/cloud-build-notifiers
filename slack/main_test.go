a20274971d8549227effeaa98e88cdba8579545e
Aric1088 <ariczhuang@gmail.com>
2022-05-18 19:53:34 +0000

    add bindings/templating to the Slack Notifier

--- slack/main_test.go
+++ slack/main_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"testing"
+	"text/template"
 
+	"github.com/GoogleCloudPlatform/cloud-build-notifiers/lib/notifiers"
 	"github.com/google/go-cmp/cmp"
 	"github.com/slack-go/slack"
 	cbpb "google.golang.org/genproto/googleapis/devtools/cloudbuild/v1"
@@ -10,28 +12,84 @@ import (
 
 func TestWriteMessage(t *testing.T) {
 	n := new(slackNotifier)
-	b := &cbpb.Build{
+
+	blockKitTemplate := `[
+		{
+		  "type": "section",
+		  "text": {
+			"type": "mrkdwn",
+			"text": "Build Status: {{.Build.Status}}"
+		  }
+		},
+		{
+		  "type": "divider"
+		},
+		{
+		  "type": "section",
+		  "text": {
+			"type": "mrkdwn",
+			"text": "View Build Logs"
+		  },
+		  "accessory": {
+			"type": "button",
+			"text": {
+			  "type": "plain_text",
+			  "text": "Logs"
+			},
+			"value": "click_me_123",
+			"url": "{{.Build.LogUrl}}",
+			"action_id": "button-action"
+		  }
+		}
+	  ]`
+
+	tmpl, err := template.New("blockkit_template").Parse(blockKitTemplate)
+	if err != nil {
+		t.Fatalf("failed to parse template: %v", err)
+	}
+	n.tmpl = tmpl
+	n.tmplView = &notifiers.TemplateView{Build: &notifiers.BuildView{Build: &cbpb.Build{
 		ProjectId: "my-project-id",
 		Id:        "some-build-id",
 		Status:    cbpb.Build_SUCCESS,
 		LogUrl:    "https://some.example.com/log/url?foo=bar",
-	}
+	}}}
 
-	got, err := n.writeMessage(b)
+	got, err := n.writeMessage()
 	if err != nil {
 		t.Fatalf("writeMessage failed: %v", err)
 	}
 
 	want := &slack.WebhookMessage{
-		Attachments: []slack.Attachment{{
-			Text:  "Cloud Build (my-project-id, some-build-id): SUCCESS",
-			Color: "good",
-			Actions: []slack.AttachmentAction{{
-				Text: "View Logs",
-				Type: "button",
-				URL:  "https://some.example.com/log/url?foo=bar&utm_campaign=google-cloud-build-notifiers&utm_medium=chat&utm_source=google-cloud-build",
-			}},
-		}},
+		Attachments: []slack.Attachment{{Color: "good"}},
+		Blocks: &slack.Blocks{
+			BlockSet: []slack.Block{
+				&slack.SectionBlock{
+					Type: "section",
+					Text: &slack.TextBlockObject{
+						Type: "mrkdwn",
+						Text: "Build Status: SUCCESS",
+					},
+				},
+				&slack.DividerBlock{
+					Type: "divider",
+				},
+				&slack.SectionBlock{
+					Type: "section",
+					Text: &slack.TextBlockObject{
+						Type: "mrkdwn",
+						Text: "View Build Logs",
+					},
+					Accessory: &slack.Accessory{ButtonElement: &slack.ButtonBlockElement{
+						Type:     "button",
+						Text:     &slack.TextBlockObject{Type: "plain_text", Text: "Logs"},
+						ActionID: "button-action",
+						URL:      "https://some.example.com/log/url?foo=bar",
+						Value:    "click_me_123",
+					}},
+				},
+			},
+		},
 	}
 
 	if diff := cmp.Diff(got, want); diff != "" {
